2ba83c95ccac0e3ad32a74f0247afc7b85998d6e
suciaisyahputri03-cloud <suciaisyahputri03@gmail.com>
2026-06-02 18:01:32 +0700

    menutuo file  apabila selesai menulis isi file dan membuaka fil edalam mode membaca

--- ioFileWithpath.cpp
+++ ioFileWithpath.cpp
@@ -30,5 +30,12 @@ int main()
         if (baris == "q") break;
         //menulis dan memasukkan nilai dari baris ke dalam file
         outfile << baris << endl;
+
     }
+    //menutup file apabila telah selesai di tulis
+    outfile.close();
+
+    //membuka file dalam mode membaca
+    ifstream infile;
+
 }
