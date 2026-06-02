1daea76dfc68313b8c77d283a3b1ef017ce340c4
suciaisyahputri03-cloud <suciaisyahputri03@gmail.com>
2026-06-02 17:41:35 +0700

    menambahkan penanganan exception menggunakan try

--- ExcepionStandart.cpp
+++ ExcepionStandart.cpp
@@ -3,3 +3,15 @@
 #include <array>
 using namespace std;
 
+int main()
+{
+    
+    cout << "Awal program" << endl; //penanda 1
+    try
+    {
+        //deklarasi array pada data dengan panjang index 3
+        array <int, 3> data = { 1, 2, 3 };
+        //memanggil array element ke-5 yang berarti akan terjadi eror
+        cout << data.at(5) << endl;
+    }
+}
