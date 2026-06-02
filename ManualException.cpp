96646353867fd824df5efb8dad33d811df5b05f9
suciaisyahputri03-cloud <suciaisyahputri03@gmail.com>
2026-06-02 18:46:52 +0700

    membuat isi dari try untuk pengujian eror

--- ManualException.cpp
+++ ManualException.cpp
@@ -1,2 +1,13 @@
 #include <iostream>
 using namespace std;
+
+int main()
+{
+    //membuat isi dari try untuk pengujian eror
+    try 
+    {
+        cout << "Selmat belajar dari prodi TI UMY" << endl;
+        //thow 0.5 //melempaekan sebuah integer maka
+        cout << "pernyataan tidak akan ditampilakn" << endl;
+    }
+}
