33ab1b56f375ccabf2a5c179ab762f29143bd523
suciaisyahputri03-cloud <suciaisyahputri03@gmail.com>
2026-06-02 18:05:11 +0700

    membuka file ke sebuah file dengan ekstensi dnegan kondisi jika file masih ada

--- ioFileWithpath.cpp
+++ ioFileWithpath.cpp
@@ -38,4 +38,14 @@ int main()
     //membuka file dalam mode membaca
     ifstream infile;
 
+    //menunjuk ke sebuah file dengan ekstensi .txt
+    infile.open(NamaFile + ".txt", ios::in);
+
+    cout << endl << ">= Membuka dan membaca file" << endl;
+    //kondisi jika file ada
+    if (infile.is_open())
+    {
+        
+    }
+
 }
