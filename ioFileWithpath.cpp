2fc47150304c7eb5e358f375616759c3b3ed0bd7
suciaisyahputri03-cloud <suciaisyahputri03@gmail.com>
2026-06-02 17:50:49 +0700

    membuat input untuk menambahakan nama file

--- ioFileWithpath.cpp
+++ ioFileWithpath.cpp
@@ -7,5 +7,9 @@ int main()
 {
     
     //deklarasi variabel baris dan NamaFile
-    string baris, Namafile;
+    string baris, NamaFile;
+
+    //membuat ibput untuk menangkap nama file
+    cout << "Masukkan nama file : ";
+    cin >> NamaFile;
 }
