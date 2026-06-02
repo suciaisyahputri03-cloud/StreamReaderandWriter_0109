76c14412e5b4a9053ceb9367e0e1cef331ca798a
suciaisyahputri03-cloud <suciaisyahputri03@gmail.com>
2026-06-02 18:56:11 +0700

    Menulis data input dari variabel baris ke dalam outfile

--- StreamReaderandWriter.cpp
+++ StreamReaderandWriter.cpp
@@ -19,3 +19,12 @@ int main()
     //unlimited loop untuk menulis file
     while (true)
     {
+        cout << "- ";
+        //mendapat setiap karakter dalam satu baris
+        getline(cin, baris);
+        //kondisi false jika baris hanya berisi  karakter q
+        if (baris == "q") break;
+        //menulis dan memasukkan nilai dari variabel baris ke dalam file
+        outfile << baris << endl;
+
+    }
